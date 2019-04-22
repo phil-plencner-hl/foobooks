65b798a3d642ba5fc63edcc32bfbc715f9854a12
Phil Plencner <philip_plencner@harvard.edu>
2019-04-21 22:14:38 -0400

    Misc. database related files.

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -4,10 +4,172 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use IanLChapman\PigLatinTranslator\Parser;
-
+use App\Book;
 
 class PracticeController extends Controller
 {
+    /**
+     *
+     */
+    public function practice14()
+    {
+        #Remove any/all books with an author name that includes the string “Rowling”.
+        # First get a books to delete
+        $books = Book::where('author', 'LIKE', '%Rowling%')->delete();
+        dump('Deletion complete; check the database to see if it worked...');
+    }
+
+    /**
+     *
+     */
+    public function practice13()
+    {
+        #Find any books by the author “J.K. Rowling” and update the author name to be “JK Rowling”
+        # First get a book to update
+        $books = Book::where('author', '=', 'J.K. Rowling')->update(['author' => 'JK Rowling']);;
+        dump('Update complete; check the database to confirm the update worked.');
+    }
+
+    /**
+     *
+     */
+    public function practice12()
+    {
+        #Retrieve all the books in descending order according to published date.
+        $books = Book::orderBy('published_year', 'desc')->get();
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice11()
+    {
+        #Retrieve all the books in alphabetical order by title.
+        $books = Book::orderBy('title')->get();
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice10()
+    {
+        #Retrieve all the books published after 1950.
+        $books = Book::where('published_year', '>', '1950')->get();
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice9()
+    {
+        # Retrieve the last 2 books that were added to the books table.
+        $books = Book::orderBy('created_at', 'desc')->limit(2)->get();
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice8()
+    {
+        # First get a book to delete
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump('Did not delete- Book not found.');
+        } else {
+            $book->delete();
+            dump('Deletion complete; check the database to see if it worked...');
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice7()
+    {
+        # First get a book to update
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump("Book not found, can't update.");
+        } else {
+            # Change some properties
+            $book->title = 'The Really Great Gatsby';
+            $book->published_year = '2025';
+
+            # Save the changes
+            $book->save();
+
+            dump('Update complete; check the database to confirm the update worked.');
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice6()
+    {
+        $book = new Book();
+        $books = $book->where('title', 'LIKE', '%Harry Potter%')->get();
+
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function practice5()
+    {
+        $book = new Book();
+
+        # Set the properties
+        # Note how each property corresponds to a field in the table
+        $book->title = 'Harry Potter and the Sorcerer\'s Stone';
+        $book->author = 'J.K. Rowling';
+        $book->published_year = 1997;
+        $book->cover_url = 'http://prodimage.images-bn.com/pimages/9780590353427_p0_v1_s484x700.jpg';
+        $book->purchase_url = 'http://www.barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-j-k-rowling/1100036321?ean=9780590353427';
+
+        # Invoke the Eloquent `save` method to generate a new row in the
+        # `books` table, with the above data
+        $book->save();
+
+        dump($book->toArray());
+    }
 
     /**
      *
@@ -24,7 +186,7 @@ class PracticeController extends Controller
      */
     public function practice2()
     {
-        return 'Need help? Email us at '.config('mail.supportEmail');
+        return 'Need help? Email us at ' . config('mail.supportEmail');
     }
 
     /**
@@ -55,7 +217,6 @@ class PracticeController extends Controller
 
             # Invoke the requested method if it exists; if not, throw a 404 error
             return (method_exists($this, $method)) ? $this->$method() : abort(404);
-
         } # If no `n` is specified, show index of all available methods
         else {
             # Build an array of all methods in this class that start with `practice`
@@ -67,7 +228,6 @@ class PracticeController extends Controller
 
             # Load the view and pass it the array of methods
             return view('practice')->with(['methods' => $methods]);
-
         }
     }
 }
